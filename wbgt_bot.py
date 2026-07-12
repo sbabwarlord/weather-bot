a92fb10403c4ffa29c10f8235b47cd5c4ac9e63c
sbabwarlord <sbabwarlord206@gmail.com>
2026-07-12 10:10:57 +0800

    Update wbgt_bot.py

--- wbgt_bot.py
+++ wbgt_bot.py
@@ -55,48 +55,53 @@ def get_message(status,uv):
         "Risk Level: *HIGHTE*\n\n"
         Reduce prolonged exposure to the sun"
     )
-    
-def get_uv_level(uv_value: int):
-    if uv_value <= 2:
-        return "Low", "No protection needed"
-    elif uv_value <= 5:
-        return "Moderate", "Some protection against sunburn is needed"
-    elif uv_value <= 7:
-        return "High", "Some protection against sunburn is needed"
-    elif uv_value <= 10:
-        return "Very High", "Extra protection against sunburn is needed"
-    else:
-        return "Extreme", "Extra protection against sunburn is needed"
 
-def fetch_uv():
-    response = requests.get(UV_API, timeout=10)
-    response.raise_for_status()
-    data = response.json()
-    index_list = data["data"]["records"][0]["index"]
-    latest = index_list[0]
-    return latest["value"]
-
-def format_message(uv_value):
-    uv_level, uv_advice = get_uv_level(uv_value)
-
-    return (
-        f"☀️ *UV Index in Singapore* 🇸🇬\n\n"
-        f"UV Index: *{uv_value}* ({uv_level})\n"
-        f"Advisory: _{uv_advice}_\n\n"
-        f"━━━━━━━━━━━━━━━\n"
+     elif status == "VERY HIGH";
+        return (
+            "*UV Index in Singapore* 🇸🇬\n\"
+        f"Current UV Index: *{uv}*\n"
+        "Risk Level: *VERY HIGH*\n\n"
+        Extra sun protection is strongly recommended"
     )
+
+     elif status == "EXTREME";
+        return (
+            "*UV Index in Singapore* 🇸🇬\n\"
+        f"Current UV Index: *{uv}*\n"
+        "Risk Level: *EXTREME*\n\n"
+        Avoid outdoor activities where possible"
+    )
+    
+def load_previous_status():
+    if os.path.exists(StATE_FILE):
+        with open(STATE_FILE, "r") as f:
+            return f.read().strip()
+    return None
+
+def save_status(status):
+    with open(STATE_FILE,"w") as f:
+        f.write(status)
 
 async def main():
-    uv_value = fetch_uv()
-    message = format_message(uv_value)
-    bot = Bot(token=TELEGRAM_TOKEN)
-    await bot.send_message(
-        chat_id=CHANNEL_ID,
-        text=message,
-        parse_mode="Markdown",
-       # message_thread_id=THREAD_ID
-    )
-    print("Sent.")
+    uv = fetch_uv()
+    current_status = get_uv_status(uv)
+    previous_status = load_previous_status()
+
+    if current_status !== previous_status:
+
+        if not (previous_status is None and current_status == "LOW"):
+
+            bot = Bot(token=TELEGRAM_TOKEN)
+
+            await bot.send_message(
+                chat_id=CHANNEL_ID,
+                text=get_message(current_status,uv),
+                parse_mode="Markdown",
+            )
+
+            print(f"Notificaiton sent: {current_status}")
+
+    save_status(current_status)
 
 if __name__ == "__main__":
     asyncio.run(main())
